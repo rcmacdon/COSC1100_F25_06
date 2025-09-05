320b94df7541d41557ad693cf7a9b10654204a6e
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-09-05 13:56:06 -0400

    Week 1 Class 2

---
+++ Week01/helloworld.py
@@ -0,0 +1,9 @@
+# -------------------------------------
+# Title:        Hello World Application
+# Author:       Clint MacDonald
+# Date:         Sept. 4, 2025
+# Purpose:      To create our first python program
+# -------------------------------------
+
+# Output some text to the console window
+print("Hello World")
---
+++ Week01/test2.py
@@ -0,0 +1,14 @@
+# -------------------------------------
+# Title:        Who am I example
+# Author:       Clint MacDonald
+# Date:         Sept 4, 2025
+# Purpose:      Use user input for the first time
+# -------------------------------------
+
+# Prompt user for their name and wait for their response
+userName = input("Please enter your name: ")
+
+# having received the input, print it back to them!
+print("Hello, " + userName + "!")
+# the + symbol here is for String Concatenation
+
