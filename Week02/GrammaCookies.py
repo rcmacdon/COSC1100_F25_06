ed39c2353b54c5e393abec197b39c6bc15df6909
rcmacdon <clint.macdonald@durhamcollege.ca>
2025-09-09 10:48:31 -0400

    Intro to Pseudocode

---
+++ Week02/GrammaCookies.py
@@ -0,0 +1,39 @@
+'''
+------------------------------------
+Title:      Doing Gramma's Cookies as Pseudocode
+Author:     Clint MacDonald
+Date:       Sept. 9, 2025
+Purpose:    Learning to write pseudocode 
+------------------------------------
+'''
+
+# import any required libraries
+
+# Search for and gather potential recipies
+
+# loop through recipies until one choice is made
+
+# GIVEN A SINGLE RECIPE
+# check if ingredients are available
+# Obtain missing ingredients
+ 
+# preheat the oven
+ 
+# get tools and bowls and cooking sheet
+
+# Mix ingredients
+
+# prepare cookie sheet
+
+# make individual cookies
+
+# cook cookies with a timer
+    # put cookies in oven
+    # set timer
+    # wiat for timer
+    # check if cookies done
+
+# let cookies stand 5 minutes
+# impatience, I'm hungry
+# yum yum cookie monster time!
+
